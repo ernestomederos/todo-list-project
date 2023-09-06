409ecc9cfacdf7726b02e227661d5851d82df714
Ernesto Mederos <ernesto.mederosgarcia@dci.education>
2023-09-07 00:00:12 +0200

    componetizing some code

--- src/App.js
+++ src/App.js
@@ -1,53 +1,34 @@
 import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 
 
 
 function App(props) {
+  const taskList = props.tasks.map((task) => (
+    <Todo 
+    id={task.id} 
+    name={task.name} 
+    completed={task.completed} 
+    key={task.id} />
+  ));
+
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form/>
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
       </div>
       <h2 id="list-heading">3 tasks remaining</h2>
       <ul
-        // role="list"
+        role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-        <Todo name="Eat" />
-        <Todo name="Sleep" />
-        <Todo name="Repeat" />
+        {taskList}
 
       </ul>
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,8 +5,8 @@ export default function Todo(props) {
     return (
         <li className="todo stack-small">
             <div className="c-cb">
-                <input id="todo-0" type="checkbox" defaultChecked={true} />
-                <label className="todo-label" htmlFor="todo-0">
+                <input id="todo-0" type="checkbox" defaultChecked={props.completed} />
+                <label className="todo-label" htmlFor={props.id}>
                     {props.name}
                 </label>
             </div>
